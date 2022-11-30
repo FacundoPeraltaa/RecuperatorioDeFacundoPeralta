05a81873b28266c0863cd3d78870256884ef0d9a
facup <facup@DESKTOP-CP1JQNP>
2022-11-30 20:34:40 +0100

    Agregar archivos de proyecto.

---
+++ WinFormsEjercicioMultiplicar/WinFormsEjercicioMultiplicar/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace WinFormsEjercicioMultiplicar
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Numero1TextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Numero2TextBox = new System.Windows.Forms.TextBox();
+            this.MultiplicarButton1 = new System.Windows.Forms.Button();
+            this.Salir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(103, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Numero1:";
+            // 
+            // Numero1TextBox
+            // 
+            this.Numero1TextBox.Location = new System.Drawing.Point(201, 53);
+            this.Numero1TextBox.Name = "Numero1TextBox";
+            this.Numero1TextBox.Size = new System.Drawing.Size(150, 31);
+            this.Numero1TextBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(103, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Numero2:";
+            // 
+            // Numero2TextBox
+            // 
+            this.Numero2TextBox.Location = new System.Drawing.Point(201, 113);
+            this.Numero2TextBox.Name = "Numero2TextBox";
+            this.Numero2TextBox.Size = new System.Drawing.Size(150, 31);
+            this.Numero2TextBox.TabIndex = 3;
+            // 
+            // MultiplicarButton1
+            // 
+            this.MultiplicarButton1.Location = new System.Drawing.Point(201, 203);
+            this.MultiplicarButton1.Name = "MultiplicarButton1";
+            this.MultiplicarButton1.Size = new System.Drawing.Size(112, 34);
+            this.MultiplicarButton1.TabIndex = 4;
+            this.MultiplicarButton1.Text = "Multiplicar";
+            this.MultiplicarButton1.UseVisualStyleBackColor = true;
+            this.MultiplicarButton1.Click += new System.EventHandler(this.MultiplicarButton1_Click);
+            // 
+            // Salir
+            // 
+            this.Salir.Location = new System.Drawing.Point(801, 516);
+            this.Salir.Name = "Salir";
+            this.Salir.Size = new System.Drawing.Size(112, 34);
+            this.Salir.TabIndex = 5;
+            this.Salir.Text = "SALIR";
+            this.Salir.UseVisualStyleBackColor = true;
+            this.Salir.Click += new System.EventHandler(this.Salir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1246, 670);
+            this.ControlBox = false;
+            this.Controls.Add(this.Salir);
+            this.Controls.Add(this.MultiplicarButton1);
+            this.Controls.Add(this.Numero2TextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Numero1TextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Multiplicar ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Numero1TextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Numero2TextBox;
+        private System.Windows.Forms.Button MultiplicarButton1;
+        private System.Windows.Forms.Button Salir;
+    }
+}
+
---
+++ WinFormsEjercicioMultiplicar/WinFormsEjercicioMultiplicar/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsEjercicioMultiplicar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int numero1;
+        private int numero2;
+        private void MultiplicarButton1_Click(object sender, EventArgs e)
+        {
+            numero1 = int.Parse(Numero1TextBox.Text);
+            numero2 = int.Parse(Numero2TextBox.Text);
+            var Multiplicar = numero1 * numero2;
+            MessageBox.Show(Multiplicar.ToString(), " Resultado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+  
+
+            Numero1TextBox.Clear();
+            Numero2TextBox.Clear();
+            Numero1TextBox.Focus();
+        }
+
+
+
+
+                private void Salir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
